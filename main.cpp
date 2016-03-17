885eba2fb9c7c5481baf0f57ed18a2a3b2f7afdc
Mateusz-str <226149@student.pwr.edu.pl>
2016-03-17 19:35:22 +0100

    P2

--- main.cpp
+++ main.cpp
@@ -1,32 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <assert.h>
 #include <time.h>
 
-//int gcd (int a, int b);
+int gcd (int a, int b);
 
 struct fraction {
 int nominator;
 int denominator;
+fraction shorten(){
+	fraction s;
+	if(nominator==0){
+		s.nominator=nominator;
+		s.denominator=1;
+	}
+	
+	
+	else{
+		s.nominator=nominator/gcd(nominator,denominator);
+		s.denominator=denominator/gcd(nominator,denominator);
+	}
+	
+return s;	
+}
 bool is_correct(){
-denominator  && nominator<denominator ? true:false ;
+return denominator && abs(nominator)<=abs(denominator) ? true:false;
 }
 };
 
 
 int main(int argc, char **argv)
 {
+	srand(time(NULL));
 	fraction *fractions = new fraction[atoi(argv[1])];
 	for(int i=0;i<atoi(argv[1]);i++){
 		fractions[i].nominator=rand()%19-9;
 		fractions[i].denominator=rand()%19-9;
+		do{
+			/*if(fractions[i].is_correct()==false)*/
+				fractions[i].nominator=rand()%19-9;
+				fractions[i].denominator=rand()%19-9;
+		}while(fractions[i].is_correct()==false); 
 	}
 	
-	for (int i = 0; i < atoi(argv[1]); assert(fractions[i++].is_correct()))
+	for (int i = 0; i < atoi(argv[1]);assert(fractions[i++].is_correct()))
 		printf("[%i] %2i / %2i\n", 
 			i, 
 			fractions[i].nominator, 
 			fractions[i].denominator);
-	
+	fraction s= fractions[3].shorten();
+	printf("[3] %2i/ %2i\n",s.nominator,s.denominator);
+
 delete []fractions ;
+}
+
+int gcd(int a, int b){
+	  if(b!=0)
+      return gcd(b,a%b);
+ 
+    return a;
 }
