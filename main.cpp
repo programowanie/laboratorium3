cb12477bdd929ea63a4695477ae2a4262998573e
Zinzoo <frytek666@gmail.com>
2016-03-17 16:42:28 +0100

    Moje ziemniaki z lab2

--- main.cpp
+++ main.cpp
@@ -1,17 +1,37 @@
 #include <stdio.h>
 #include <assert.h>
 #include <time.h>
+#include <stdlib.h>
 
 //int gcd (int a, int b);
 
 struct fraction
 {
+	int nominator, denominator;
 
+	bool is_correct()
+	{
+		return(denominator!=0 && nominator<denominator) ? true : false;
+	}
 };
 
 int main(int argc, char **argv)
 {
 	fraction *fractions;
+
+	int n=atoi(argv[1]);
+
+	fractions = new fraction[n];
+
+	srand(time(NULL));
+
+	for( int i=0; i<n; i++)
+	{
+		fractions[i].nominator=rand()% 19-9;
+		fractions[i].denominator=rand()% 19-9;
+	}
+
+	for(int i=0; i)
 
 	/*
 	for (int i = 0; i < n; assert(fractions[i++].is_correct()))
