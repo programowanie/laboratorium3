206fddd924b68e27d13a300da41e7dc3213fada7
student <student@debian>
2016-03-17 16:47:12 +0100

    praca na zajeciach

--- main.cpp
+++ main.cpp
@@ -6,12 +6,22 @@
 
 struct fraction
 {
-
+	int nominator;
+	int denominator;
+	bool is_correct;
 };
 
 int main(int argc, char **argv)
 {
-	fraction *fractions;
+	srand(time(NULL));
+	int n=atoi(argv[1]);
+	int fraction *fractions=new int fraction.denominator[n];
+	int fraction *fractions=new int fraction.nominator[n];
+	for(int i=0; i<n; i++)
+	{
+		fraction[i].nominator=(rand()%19)-9;
+		fraction[i].denominator=(rand()%19)-9;
+	}
 
 	/*
 	for (int i = 0; i < n; assert(fractions[i++].is_correct()))
