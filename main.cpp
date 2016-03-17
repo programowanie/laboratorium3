cf4142de53ffb73a2f7545f98d8c4ea19ec4908a
Xsenonn <225974@student.pwr.edu.pl>
2016-03-17 16:42:14 +0100

    2Laborki

--- main.cpp
+++ main.cpp
@@ -6,18 +6,24 @@
 
 struct fraction
 {
+	int nominator;
+	int denominator;
+	bool is_correct()
+	{
+		 return denominator != 0 || nominator > denominator  ? true : false
+
+	}
 
 };
+
 
 int main(int argc, char **argv)
 {
 	fraction *fractions;
-
-	/*
+	int n = atoi(argv[2]);
 	for (int i = 0; i < n; assert(fractions[i++].is_correct()))
 		printf("[%i] %2i / %2i\n", 
 			i, 
 			fractions[i].nominator, 
 			fractions[i].denominator);
-	*/
 }
