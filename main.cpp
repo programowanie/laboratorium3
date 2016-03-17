84e25053dc27380792988d37cb086ffaae110352
terry1996 <226029@student.pwr.edu.pl>
2016-03-17 16:42:57 +0100

    Projekt 1

--- main.cpp
+++ main.cpp
@@ -6,18 +6,30 @@
 
 struct fraction
 {
+	int nominator, denominator;
+	bool is_correct(){ return (denominator!=0 and nominator>denominator) ? TRUE : FALSE}
 
 };
 
 int main(int argc, char **argv)
 {
-	fraction *fractions;
+	int n=atoi(arv[1]);
+	fraction *fractions=new fraction[n];
+	srand(time(NULL));
+	for(int i=0;i<n;i++){
+		
+		do{
+			fraction[i]=rand%19-9;
+		}while(fraction::is_correct());
+	}
+	
 
-	/*
+
+	
 	for (int i = 0; i < n; assert(fractions[i++].is_correct()))
 		printf("[%i] %2i / %2i\n", 
 			i, 
 			fractions[i].nominator, 
 			fractions[i].denominator);
-	*/
+	
 }
