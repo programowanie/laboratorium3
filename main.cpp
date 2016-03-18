629a70cb36562de099618377e3661a5638329682
KamilKot <226221@student.pwr.edu.pl>
2016-03-18 10:46:51 +0100

    labki2

--- main.cpp
+++ main.cpp
@@ -6,14 +6,33 @@
 //komentarz
 
 struct fraction
-{
-
+{ int nominator;
+  int denominator;
+  
 };
+
+bool is_correct
+{
+   {return denominator != 0 && nominator < denominator 
+   	? true
+   	: false;}
+}
 
 int main(int argc, char **argv)
 {
+	int n;
+  n=atoi(argv[1]);
+  srand(time(NULL));
 	fraction *fractions;
+	*fractions = new fraction [n];
+	for (int i=0;i<n;i++)
+	{
+		int s;
+		s=(rand()%19)-9;
+		fraction[i]=s;
 
+	}
+  assert(is_correct==true)
 	/*
 	for (int i = 0; i < n; assert(fractions[i++].is_correct()))
 		printf("[%i] %2i / %2i\n", 
