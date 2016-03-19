c04a54b255fa40bac5fba5ef484884f7463818df
Frynio <226228@student.pwr.edu.pl>
2016-03-19 20:10:09 +0100

    Rozwiązanie

--- main.cpp
+++ main.cpp
@@ -1,23 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <time.h>
 
-//int gcd (int a, int b);
+int gcd(int x, int y);
 
-struct fraction
+struct fraction	{
+				int n,d;
+				bool is_correct(){return d!=0?(abs(n)<abs(d)?true:false):false;};
+				void shorten(){int g = gcd(d, n);g!=1?(n/=g,d/=g):g;d<0?(n=-n,d=abs(d)):d;};
+				};
+
+
+
+int main(int argc, char** argv)
 {
+	int n=atoi(argv[1]);
+	fraction *fractions = new fraction[n];
+	srand(time(NULL));
+	for (int i=0; i<n; i++)
+	{
+		do{
+		fractions[i].n=rand() % 19 + (-9);
+		fractions[i].d=rand() % 19 + (-9);
+		}while (fractions[i].is_correct()!=true);
+		assert(fractions[i].is_correct());
+		fractions[i].shorten();
+		fractions[i].n==0?printf("0\n"):printf("%d/%d\n", fractions[i].n, fractions[i].d);
+	}
+	delete []fractions;
+}
 
-};
-
-int main(int argc, char **argv)
+int gcd(int x, int y)
 {
-	fraction *fractions;
-
-	/*
-	for (int i = 0; i < n; assert(fractions[i++].is_correct()))
-		printf("[%i] %2i / %2i\n", 
-			i, 
-			fractions[i].nominator, 
-			fractions[i].denominator);
-	*/
+	int z;
+	while (y!=0){z=x%y;x=y;y=z;}
+	return x;
 }
