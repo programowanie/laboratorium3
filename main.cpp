8711ebd9ce517c473b07729ed7e57a3983173242
JakubJachowski <226111@student.pwr.edu.pl>
2016-03-20 13:35:42 +0100

    czas na obiad

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <time.h>
 
@@ -6,18 +7,33 @@
 
 struct fraction
 {
-
+	int nominator, denominator;
+	bool is_correct()
+	{
+		return ((denominator!=0)&&(nominator<denominator) ? true : false);
+	}
 };
 
 int main(int argc, char **argv)
 {
 	fraction *fractions;
+	int n=atoi(argv[1]);
+	fractions= new fraction [n];
+	srand(time(NULL));
 
-	/*
+	for(int i=0;i<n;i++)
+	{
+		do
+		{
+			fractions[i].nominator=-9+rand()%+19;
+			fractions[i].denominator=-9+rand()%+19;
+		}while(!(fractions[i].is_correct()));
+	}
+
 	for (int i = 0; i < n; assert(fractions[i++].is_correct()))
 		printf("[%i] %2i / %2i\n", 
 			i, 
 			fractions[i].nominator, 
 			fractions[i].denominator);
-	*/
+	
 }
