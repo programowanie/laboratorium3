87411815aa930eec590398f7ac6eb25cc7d466a3
Khaydarin <khaydarinpl@gmail.com>
2016-03-21 03:17:30 +0100

    fractions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
 #include <time.h>
-#include <cstdlib>
-//int gcd (int a, int b);
+#include <stdlib.h>
+#include <math.h>
+
+int gcd (int a, int b);
 
 struct fraction
 {
@@ -11,6 +13,15 @@ struct fraction
 	bool is_correct()
 	{
 		return((denominator!=0) && (abs(numerator)<abs(denominator))?true:false);
+	}
+	void shorten()
+	{
+		if(numerator!=0)
+		{
+			int d=gcd(numerator, denominator);
+			numerator=numerator/d;
+			denominator=denominator/d;
+		}
 	}
 };
 
@@ -38,5 +49,47 @@ int main(int argc, char **argv)
 			fractions[i].numerator, 
 			fractions[i].denominator);
 
+
+
+		fractions[i].shorten();
+		printf("[%i] %2i / %2i\n", 
+			i, 
+			fractions[i].numerator, 
+			fractions[i].denominator);
+
+	}
+}
+	//a=numerator, b=denominator
+int gcd (int a, int b)
+{
+	a=abs(a);
+	b=abs(b);
+	int gcd=1;
+
+
+	if(a==0)
+	{
+		return 0;
+
+	}
+	
+	else if(0==(b%a))
+	{
+		return a;
+	}
+	else
+	{
+		int step=2;
+
+		while(step<=sqrt(b))
+		{
+
+			if((a%step==0) && (b%step==0))
+				gcd*=step;
+
+			step++;
+		}
+
+		return gcd;
 	}
 }
