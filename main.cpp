9396104c52bc14ac06764e352e7d2a106af3a7e9
KamilKluba <226016@student.pwr.edu.pl>
2016-03-22 17:11:01 +0100

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
 #include <stdio.h>
 #include <assert.h>
-#include <time.h>
+#include <ctime>
+
+using namespace std;
 
 //int gcd (int a, int b);
 
@@ -8,11 +11,14 @@ struct fraction
 {
 	int nominator, denominator;
 	
-	bool is_correct(denominator!=0 && nominator<denominator)
+	bool is_correct (int nominator, int denominator)
 	{
-		return true;
+		if (denominator!=0 && abs(nominator) < abs(denominator));
+			return true;
+		
+		return false;
+		
 	}
-
 
 };
 
@@ -20,16 +26,28 @@ int main(int argc, char **argv)
 {
 	int n;
 
-	n = atoi(argv[1])
+	n=atoi(argv[1]);
 
-	fraction *fractions;
-	fractions = new fraction [n];
+	fraction *fractions = new fraction[n];
 
-	/*
-	for (int i = 0; i < n; assert(fractions[i++].is_correct()))
-		printf("[%i] %2i / %2i\n", 
-			i, 
-			fractions[i].nominator, 
-			fractions[i].denominator);
-	*/
+	srand(time(NULL));
+	int a;
+
+	for (int j=0; j<n; j++)
+	{
+			do
+			{
+				(fractions + j)-> nominator = rand() % 19 - 9;
+				(fractions + j)-> denominator = rand() % 19 - 9;
+
+			} while (fractions[j].denominator==0 || abs(fractions[j].nominator) > abs(fractions[j].denominator));
+	}
+
+	
+	for (int i = 0; i < n; assert(fractions[i].is_correct(fractions[i].nominator, fractions[i].denominator)))
+		{
+			printf("[%i] %2i / %2i\n",i,fractions[i].nominator,fractions[i].denominator);
+			i++;
+		}
+	delete[] fractions;
 }
