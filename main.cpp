4d4f91562a5ef82f58cc82b08fa396102c38e1bf
226003 <226003.student.pwr.edu.pl>
2016-03-22 21:03:16 +0100

    226003 Rozwiazanie

--- main.cpp
+++ main.cpp
@@ -2,40 +2,80 @@
 #include <assert.h>
 #include <cstdlib>
 #include <time.h>
+#include <math.h>
 
-//int gcd (int a, int b);
+int gcd (int a, int b);
+
 
 struct fraction
 {
 	int nominator, denominator;
 	bool is_correct();
-	
+	void shorten();
 };
 
 int main(int argc, char **argv)
 {
 	int n = atoi(argv[1]);
+	srand(time(NULL));	
 	
 	
-	fraction *fractions;
-	*fractions = new franction[n];
+	fraction *fractions = new fraction [n];
 	for (int i =0; i<n; i++)
 	{
-		fractions[i]=rand()% 9+(-9);
+
+		do
+		{
+			fractions[i].nominator=rand()% 19+(-9);
+			fractions[i].denominator=rand()% 19+(-9);
+		}
+		while(! fractions[i].is_correct());
+		fractions[i].shorten();
 	}
-	/*
+	
 	for (int i = 0; i < n; assert(fractions[i++].is_correct()))
 		printf("[%i] %2i / %2i\n", 
 			i, 
 			fractions[i].nominator, 
 			fractions[i].denominator);
-	*/
+	
 }
 
-bool is_correct()
+bool fraction::is_correct()
 	{
-		if()
-		{
-
-		}
+		return (abs(nominator)<abs(denominator)&&denominator!=0) ? true : false;	
 	}
+
+void fraction::shorten()
+	{
+		int divider=gcd(nominator, denominator);
+		
+		nominator 	/=divider;
+		denominator	/=divider;
+		if(denominator<0)
+			{
+				nominator*=-1;
+				denominator*=-1;
+			}
+	}
+
+int gcd(int a, int b)
+{
+	a=abs(a);
+	b=abs(b);
+    	printf("a= %i, b= %i\n",a,b );
+
+    while(a!=b&&a!=0)
+
+    {
+       if(a>b)
+           a-=b;
+       else
+           b-=a;
+    	printf("a= %i, b= %i\n",a,b );
+     }
+    if(a!=0)
+    	return a;
+	else
+		return 1;
+}
