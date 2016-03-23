8a23b2352e7fba20dfcde64212354e839fab0d7f
226100 <piotrchlystek@hotmail.com>
2016-03-23 10:58:26 +0100

    Final version

--- main.cpp
+++ main.cpp
@@ -3,45 +3,72 @@
 #include <time.h>
 #include <cstdlib>
 
-//int gcd (int a, int b);
+int gcd (int a, int b);
 
 struct fraction
 {
 	int nominator, denominator;
-	
-	bool is_correct()
-	{
-		//((denominator != 0 && nominator < denominator) ? return 1 : return 0);
-		return ((denominator != 0 && nominator < denominator) ? true : false);
-	};
+
+	bool is_correct();
+	void shorten();
 };
 
 int main(int argc, char **argv)
 {
 	fraction *fractions;
-	
+
 	if (argc == 2)
 	{
 		int n = atoi(argv[1]);
 		fractions = new fraction [n];
 
-		
 		srand(time(NULL));
 		for (int i = 0; i < n; i++)
 		{
-			fractions[i].nominator = (rand() % 19 - 10);
-			fractions[i].denominator = (rand() % 19 - 10);
+		    do
+            {
+                fractions[i].nominator = (rand() % 19) - 9;
+                fractions[i].denominator = (rand() % 19) - 9;
+            } while (!fractions[i].is_correct());
+
+            fractions[i].shorten();
 		}
 
-		for (int i = 0; i < n; i++)
-
 		for (int i = 0; i < n; assert(fractions[i++].is_correct()))
-			printf("[%i] %2i / %2i\n", 
-				i, 
-				fractions[i].nominator, 
+			printf("[%i] %2i / %2i\n",
+				i,
+				fractions[i].nominator,
 				fractions[i].denominator);
+
+        delete [] fractions;
 	}
 
-	delete [] fractions;
 	fractions = NULL;
+}
+
+int gcd(int a, int b)
+{
+    int c;
+
+    while (b != 0)
+    {
+        c = a % b;
+        a = b;
+        b = c;
+    }
+
+    return a;
+}
+
+bool fraction::is_correct()
+{
+    return ((denominator != 0 && abs(nominator) < abs(denominator)) ? true : false);
+}
+
+void fraction::shorten()
+{
+    int divider = gcd(nominator, denominator);
+
+    nominator /= divider;
+    denominator /= divider;
 }
