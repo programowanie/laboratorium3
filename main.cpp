267dfde5ecc380599a035b798d8b238bd59edc2c
Jakub Stypułkowski <stypulkowski.jakub@gmail.com>
2016-03-25 00:37:15 +0100

    Wszystkie polecenia, wersja na 5 + ADNOTACJA.
    Na końcu komentarz z obszernym wyjasnieniem.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
+// Na dole w komentarzu istotne informacje. 
+
 #include <stdio.h>
 #include <assert.h>
 #include <time.h>
+#include <cmath>
 
 #include <stdlib.h> //[S]Rand
 
@@ -11,7 +14,7 @@ int gcd (int a, int b) {
 		a = b;
 		b = c;
 	}
-	return a; // dzielnik via euklides.
+	return abs(a); // dzielnik via euklides, patrz adnotacja.
 }
 
 struct fraction
@@ -37,9 +40,12 @@ int main(int argc, char **argv)
 		do {
 			frs[i].nominator = rand() % 18 -9;
 			frs[i].denominator = rand() % 18 -9;
+			//printf("Licznik: %d Mianownik: %d\n", frs[i].nominator, frs[i].denominator); patrz adnotacja
+			frs[i].shorten(frs[i].nominator, frs[i].denominator);
+			//printf("Licznik: %d Mianownik: %d\n", frs[i].nominator, frs[i].denominator); patrz adnotacja
 		}
 		while(!(frs[i].is_correct(frs[i].nominator, frs[i].denominator)));
-		printf("%d/%d\n", frs[i].nominator, frs[i].denominator);
+		printf("%d/%d\n", frs[i].nominator, frs[i].denominator); // patrz adnotacja
 	}
 	for (int i = 0; i < n; assert(frs[i++].is_correct(frs[i].nominator, frs[i].denominator))) {
 		printf("[%i] %2i / %2i\n", 
@@ -48,3 +54,15 @@ int main(int argc, char **argv)
 			frs[i].denominator);
 	}
 }
+
+/*	BARDZO WAZNE: w linii 15 zwracany jest MODUL z dzielnika. Wykorzystano funkcje cmath.
+*	Bez tego program rowniez przechodzi testy i sie normalnie uruchamia, ale zawiera pewien blad (zakladajac powyzsza implementacje).
+*	Powoduje go algorytm Euklidesa (raczej nie moj sposob skracania ulamkow, w koncu i tak trzeba by bylo dzielic przez NWD), gdy pierwszy argument jest liczbą ujemna.
+*	Wersja z odejmowaniem nie uruchamia sie w ogole, a z operacjami modulo zwraca dzielnik ujemny.
+*	W funkcji shorten po prostu dzielimy licznik i mianownik przez otrzymany wspolny dzielnik. Ale co, gdy jest on ujemny?
+*	Z wejsciowych danych: -3/6 (poprawny ulamek) zrobi 1/-2 (niespelniajacy zalozen is_correct) i bedzie losowal kolejne liczby...
+*	...mimo tego, ze na wejsciu byly dobre dane. W koncu wylosowane zostana liczby spelniajace test i bedzie to niezauwazone.
+*	Trzeba wiec dzielic przez modul z NWD - mozna to zrobic w funkcji shorten, mozna w funkcji gcd.
+*
+*	Przepraszam, ze sie rozpisalem, ale swiadomosc, ze cos nie do konca jest dobrze, ale olac to bo przechodzi testy, srednio mnie satysfakcjonuje :)
+*/
