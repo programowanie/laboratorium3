f023bf0df2399eb1b15019c39d0a6b79998f55de
Jakub Stypułkowski <stypulkowski.jakub@gmail.com>
2016-03-24 23:42:48 +0100

    Z funkcją gcd + shorten - NOT DONE!

--- main.cpp
+++ main.cpp
@@ -4,14 +4,27 @@
 
 #include <stdlib.h> //[S]Rand
 
-
-//int gcd (int a, int b);
+int gcd (int a, int b) {
+	int c;
+	while(b!=0) {
+		c = a%b;
+		a = b;
+		b = c;
+	}
+	return a; // dzielnik via euklides.
+}
 
 struct fraction
 {
 	int nominator, denominator;
 	bool is_correct(int nom, int denom) {
 		return (denom != 0 && nom < denom) ? true : false;
+	}
+	void shorten (int &a, int &b) {
+		int x = a/gcd(a, b);
+		int y = b/gcd(a, b);
+		a = x;
+		b = y;
 	}
 };
 
@@ -30,7 +43,7 @@ int main(int argc, char **argv)
 	}
 	for (int i = 0; i < n; assert(frs[i++].is_correct(frs[i].nominator, frs[i].denominator))) {
 		printf("[%i] %2i / %2i\n", 
-			i, 
+			i+1, 
 			frs[i].nominator, 
 			frs[i].denominator);
 	}
