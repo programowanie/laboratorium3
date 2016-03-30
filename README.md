d23570903f65779572eed2929ad7a669321348e6
s3blag <226173@student.pwr.edu.pl>
2016-03-30 13:41:28 +0200

    Rozwiązanie

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
     - pobierz liczbę całkowitą `n`, jako argument wiersza poleceń,
     - zadeklaruj dynamiczny `n`-elementowy łańcuch typu `fraction`,
     - wypełnij go losowymi wartościami z przedziału `<-9;9>`
-
+    
 ###Ocena dobra
 - Używając funkcji `assert()`, przekazując do niej wynik funkcji `is_correct()`, sprawdź, czy wszystkie wylosowane ułamki są poprawne.
 - Uzupełnij pętlę, w której losowane były wartości ułamków o zagnieżdżoną pętlę *do while*, sprawdzającą poprawność ułamka po wygenerowaniu, aby wszystkie testy przeszły pomyślnie.
@@ -22,5 +22,5 @@
 - Napisz funkcję `gcd()`, odnajdującą największy wspólny dzielnik dwóch liczb całkowitych i użyj jej w napisanej przez Ciebie funkcji `shorten()`, będącej częścią struktury `fraction()`, do skracania ułamka.
 
 ## Dokumentacja
-- [assert()](http://www.cplusplus.com/reference/cassert/assert/)
+- [assert()](11http://www.cplusplus.com/reference/cassert/assert/)
 - [srand()](http://www.cplusplus.com/reference/cstdlib/srand/)
--- main.cpp
+++ main.cpp
@@ -1,23 +1,79 @@
 #include <stdio.h>
 #include <assert.h>
 #include <time.h>
-
-//int gcd (int a, int b);
+#include <cstdlib>
+int gcd (int a, int b);
 
 struct fraction
 {
+
+	int nominator, denominator;
+
+	bool is_correct()
+	{
+	return (denominator!=0 && abs(nominator)<abs(denominator)? true : false);//wartosc bezwzgledna, by pozbyc sie przypadku gdy mamy np. -9/-1
+	}
+	
+	void shorten()
+	{
+	int GCD=gcd(nominator,denominator);
+	nominator=nominator/GCD;
+	denominator=denominator/GCD;
+	//kosmetyka
+	if (denominator<0)
+	{
+	nominator*=(-1);
+	denominator*=(-1);
+	}
+
+	}
 
 };
 
 int main(int argc, char **argv)
 {
-	fraction *fractions;
-
-	/*
+int n;
+n=atoi(argv[1]);
+fraction *fractions= new fraction[n];
+srand(time(NULL));
+	for (int i=0;i<n;i++)
+	{
+		do
+		{
+		fractions[i].nominator=rand()%19-9;
+		fractions[i].denominator=rand()%19-9;
+		}
+		while (fractions[i].is_correct()==false);
+	//assert(fractions[i].is_correct());
+	}
+	//przed skracaniem
 	for (int i = 0; i < n; assert(fractions[i++].is_correct()))
 		printf("[%i] %2i / %2i\n", 
 			i, 
 			fractions[i].nominator, 
 			fractions[i].denominator);
-	*/
+	printf("\n");
+	//po skroceniu i kosmetyce
+		for (int i = 0; i < n; assert(fractions[i++].is_correct()))
+		{	fractions[i].shorten();
+		printf("[%i] %2i / %2i\n", 
+			i, 
+			fractions[i].nominator, 
+			fractions[i].denominator);
+	}
+	
+}
+
+int gcd (int a, int b)
+{
+    int c;
+ 
+  while(b!=0)
+    {
+    c = b;
+    b = a%b;
+    a = c;  
+    }
+ 
+    return a;
 }
