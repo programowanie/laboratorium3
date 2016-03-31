ee7459c9bbaa49ff7fdb7c4bb285b7a4dbf9aec9
anemorpheus <226199@student.pwr.edu.pl>
2016-03-31 17:35:26 +0200

    Rozwiązanie

--- main.cpp
+++ main.cpp
@@ -1,23 +1,50 @@
 #include <stdio.h>
 #include <assert.h>
 #include <time.h>
+#include <stdlib.h>
 
-//int gcd (int a, int b);
+int gcd (int a, int b) {
+	int c;
+	while (b != 0) {
+		c = a%b;
+		a = b;
+		b = c;
+	}
+	return abs(a);
+}
 
-struct fraction
-{
-
+struct fraction {
+	int nominator;
+	int denominator;
+	bool is_correct() {
+		return ((denominator!=0)&&(nominator<denominator))?true:false;
+	}
+	void shorten() {
+		int divider = gcd(nominator, denominator);
+		nominator /= divider;
+		denominator /= divider;
+	}
 };
 
-int main(int argc, char **argv)
-{
-	fraction *fractions;
-
-	/*
-	for (int i = 0; i < n; assert(fractions[i++].is_correct()))
-		printf("[%i] %2i / %2i\n", 
-			i, 
-			fractions[i].nominator, 
-			fractions[i].denominator);
-	*/
+int main(int argc, char **argv) {
+	if (argc==2) {
+		int n = atoi(argv[1]);
+		if (n>0) {
+			srand(time(NULL));
+			fraction *fractions = new fraction[n];
+			for (int i = 0; i < n; i++) {
+				do {
+					fractions[i].nominator = rand()%20-10;
+					fractions[i].denominator = rand()%20-10;
+				} while(!fractions[i].is_correct());
+				fractions[i].shorten();	
+			}
+			for (int i = 0; i < n; assert(fractions[i++].is_correct()))
+				printf("[%i] %2i / %2i\n", 
+					i, 
+					fractions[i].nominator, 
+					fractions[i].denominator);
+			delete[] fractions;
+		} else printf("Błąd: podano niepoprawną długość tablicy");
+	} else printf("Błąd: błędna długość listy argumentów");
 }
