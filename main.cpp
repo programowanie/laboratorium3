83f54e17647e6dde03f602e5ac4100ee51d162ff
Krzysztof Dombek <226093@student.pwr.edu.pl>
2016-04-01 15:29:01 +0200

    rozwiazanie

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
 #include <time.h>
+#include <cstdlib>
+#include <math.h>
 
-int gcd (int a, int b);
+int gcd(int a, int b)
 {
 	do
 	{
 		if(a > b) a = a-b;
 		else b = b-a;
-	}while(a!=b)
+	}while(a!=b);
 	return a;
 }
 
@@ -19,7 +21,7 @@ struct fraction
 
 	bool iscorrect()
 	{
-		return ( mianownik && abs(licznik) < abs(mianownik))) ? true : false;
+		return ( mianownik != 0 && abs(licznik) < abs(mianownik)) ? true : false;
 	}
 void shorten()
 {
@@ -31,21 +33,21 @@ void shorten()
 };
 
 int main(int argc, char **argv)
-{
-	fraction *fractions;
-	srand=(time(NULL));
+{	
+	
+	srand(time(NULL));
 	int n = atoi(argv[1]);
-	fraction *fraction = new fraction[n];
+	fraction *fractions = new fraction[n];
 
 	for(int i = 0; i < n; i++)
 	{
 		do
 		{
-			fraction.[i].licznik = rand() % 19 - 9;
-			fraction.[i].mianownik = rand() % 19 - 9;
+			fractions[i].licznik = (rand() % 19) - 9;
+			fractions[i].mianownik = (rand() % 19) - 9;
 
-		}while ((fraction.[i].iscorrect == false))
-		fraction.[i].shorten();
+		}while((fractions[i].iscorrect() == false));
+		fractions[i].shorten();
 	}
 
 	/*
