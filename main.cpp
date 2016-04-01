4729c02e8fccce8232f3b829b63aa05397595920
BartekCzesak <225974@student.pwr.edu.pl>
2016-04-01 20:40:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,45 +12,45 @@ struct fraction
 	int denominator;
 	bool is_correct()
 	{
-		 return (abs(denominator) != 0 || abs(numerator) > abs(denominator)  ? true : false);
+		 return denominator != 0 ? (abs(numerator) < abs(denominator) ? true : false) : false;
 
 	}
 	void shorten()
 	{
 		int divider = gcd(denominator, numerator); 
 		divider != 1 ? (numerator /= divider  , denominator /= divider) : divider;
-		denominator < 0 ? (numerator= -numerator , denominator=abs(denominator) : denominator);
+		denominator < 0 ? (numerator= -numerator , denominator=abs(denominator)) : denominator;
 	}
 };
 
-int gcd (int n, int d)
+int gcd (int numerator, int denominator)
 {
 	int pom;
-	while(b!=0)
+	while(denominator!=0)
 	{
-		pom = n % d;
-		n = d;
-		d = pom;
+		pom = numerator % denominator;
+		numerator = denominator;
+		denominator = pom;
 	}
-	return n;
+	return numerator;
 };
 
 int main(int argc, char **argv)
 {
 	srand(time(NULL));
-	fraction *fractions;
 	int n = atoi(argv[2]);
 	fraction *fractions = new fraction[n];
 	for (int i = 0; i < n; i++)
 	{
 		do
 		{
-		  fractions[i].numerator=(rand()%( 9 - ( -9 )) + 9); 
-		  fractions[i].denominator=(rand()%( 9 - ( -9 )) + 9);
-		}while(fractions[i].is_correct() = true);
+		  fractions[i].numerator=rand()% 19 + (-9);
+		  fractions[i].denominator=rand()% 19 + (-9);
+		}while(fractions[i].is_correct() != true);
 
 		assert(fractions[i].is_correct() );
 		fractions[i].shorten();
-		fractions[i].numerator == 0 ? printf(" 0 \n ") : print("%d / %d \n", fractions[i].numerator , fractions[i].denominator);
+		fractions[i].numerator == 0 ? printf(" 0 \n ") : printf("%d / %d \n", fractions[i].numerator , fractions[i].denominator);
 	}
+	delete []fractions;
 }
