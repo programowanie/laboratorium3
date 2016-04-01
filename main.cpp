5f0da12ba835e4b4355dccdb9b021306f6fa3bb4
student <student@debian>
2016-04-01 02:20:52 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,39 @@
 #include <stdio.h>
 #include <assert.h>
 #include <time.h>
-
-//int gcd (int a, int b);
+#include <cstdlib>
+#include <cmath>
 
 struct fraction
 {
+	int denominator,nominator;
+
+	bool is_correct()
+	{
+	return (abs(denominator) > abs(nominator) && denominator !=0) ?true :false;
+	};
 
 };
 
 int main(int argc, char **argv)
 {
-	fraction *fractions;
-
-	/*
+	srand(time(NULL));
+	int n=atoi(argv[1]);
+	fraction *fractions= new fraction[n];
+	for(int i=0;i<n;i++)
+	{
+		do
+		{
+		fractions[i].denominator=rand()%19-9;
+		fractions[i].nominator=rand()%19-9;
+		}while( !fractions[i].is_correct());
+		assert (fractions[i].is_correct());
+		
+	}
+		
 	for (int i = 0; i < n; assert(fractions[i++].is_correct()))
 		printf("[%i] %2i / %2i\n", 
 			i, 
 			fractions[i].nominator, 
 			fractions[i].denominator);
-	*/
 }
