bf7f1bd38c8ab68af0100ae1bf68d361d1b27686
MegaKruk <student@debian>
2016-04-26 22:12:40 +0200

    Rozwiązanie

--- main.cpp
+++ main.cpp
@@ -1,23 +1,44 @@
 #include <stdio.h>
-#include <assert.h>
+#include <cstdlib>
 #include <time.h>
+#include <assert.h>
 
-//int gcd (int a, int b);
 
-struct fraction
+struct fraction 
 {
+ 	int nominator;
+ 	int denominator;
 
+ 	bool is_correct()
+	{
+ 	if(abs(nominator) < abs(denominator) && denominator != 0)
+		return true;
+	else
+		return false;
+ 	}
+  
+ 	
 };
 
-int main(int argc, char **argv)
+int main(int argc, char **argv) 
 {
-	fraction *fractions;
+	
+	int n = atoi(argv[1]);
+	srand(time(NULL));
+	fraction *fractions = new fraction[n];
 
-	/*
-	for (int i = 0; i < n; assert(fractions[i++].is_correct()))
-		printf("[%i] %2i / %2i\n", 
-			i, 
-			fractions[i].nominator, 
-			fractions[i].denominator);
-	*/
+	for(int i = 0; i < n; i++) 
+	{
+		do {
+		fractions[i].nominator = rand()%19 - 9;
+		fractions[i].denominator = rand()%19 - 9;
+		} while(fractions[i].is_correct() != false);
+	}
+	for(int i = 0; i < n; assert(fractions[i++].is_correct())) 
+	{
+	printf("%d/%d\n", fractions[i].nominator, fractions[i].denominator);
+	}
+
+delete []fractions;
+
 }
