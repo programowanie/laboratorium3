fd93e9974ed8a75d0c92e53775c7e742fb963e47
matmrozinski <m.mrozinski123@gmail.com>
2016-04-28 18:13:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,57 @@
 #include <stdio.h>
 #include <assert.h>
 #include <time.h>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
 
 //int gcd (int a, int b);
 
 struct fraction
 {
+int licznik;
+int mianownik;
 
+bool is_correct()
+    {
+    int prawda_czy_falsz;
+    prawda_czy_falsz=(licznik<mianownik && mianownik!=0) ? 1:0;
+
+    return prawda_czy_falsz;
+    }
 };
 
 int main(int argc, char **argv)
 {
-	fraction *fractions;
 
+    int n;
+
+	 if (argc >= 2)
+  {
+    n = atoi(argv[1]);
+  }
+  else
+  {
+    cout << "Podaj liczbe calkowita n: ";
+    cin >> n;
+  }
+
+    fraction *fractions = new fraction[n];
+    srand( time( NULL ) );
+
+    for(int i=0; i<n; i++)
+    {
+    fractions[i].licznik= rand()%19 - 9;
+    fractions[i].mianownik= rand()%19 - 9;
+    }
 	/*
 	for (int i = 0; i < n; assert(fractions[i++].is_correct()))
-		printf("[%i] %2i / %2i\n", 
-			i, 
-			fractions[i].nominator, 
+		printf("[%i] %2i / %2i\n",
+			i,
+			fractions[i].nominator,
 			fractions[i].denominator);
 	*/
+
+	delete []fractions;
 }
